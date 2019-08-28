29aaf87a4876a12e32834a0d68dd32928f662bab
github username <github email address>
2019-08-27 17:16:02 -0700

    adding more content

--- Blog_ruby_CSV_library.rb
+++ Blog_ruby_CSV_library.rb
@@ -2,6 +2,10 @@ require 'csv'
 
 table = CSV.read("test.csv", headers: true)
 
+CSV::Converters[:boolean] = ->(value) { value.downcase.to_s == "true" rescue value }
 
+table = CSV.parse("1,true\n2,false\n3,false", converters: :boolean)
 
+puts table[1][1].class
 
+CSV.open('tester.csv', "wb") { |csv| csv << [1,"yes","yes"]; csv << [2,"no","no"] }
